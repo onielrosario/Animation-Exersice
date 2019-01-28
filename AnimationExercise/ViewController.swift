f27d68221bf5fd31aa63a4a65532c00c2cbe7d15
onielrosario <onielrosario@pursuit.org>
2019-01-28 12:12:50 -0500

    first commit

---
+++ AnimationExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationExercise
+//
+//  Created by Oniel Rosario on 1/28/19.
+//  Copyright © 2019 Oniel Rosario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+
+
+}
+
